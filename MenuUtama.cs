d3b3a397458b046e579febb7414bcb3c363b2426
AdnanNBachtiar <126306382+AdnanNBachtiar@users.noreply.github.com>
2023-07-03 22:07:54 +0700

    Game Tank Hunter

---
+++ MenuUtama.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUtama : MonoBehaviour
+{
+    public GameObject PanelUtama;
+    public GameObject PanelSetting;
+    public GameObject PanelStage;
+    public GameObject AboutUs;
+    public GameObject HowToPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PanelUtama.SetActive(true);
+        PanelSetting.SetActive(false);
+        PanelStage.SetActive(false);
+        AboutUs.SetActive(false);
+        HowToPlay.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
